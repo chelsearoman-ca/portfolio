5666d55b83bb1abe3e5ec1e0cedea456a16afdd4
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-01-04 21:43:42 -0500

    blue background

---
+++ dev/scripts/main.js
@@ -0,0 +1,50 @@
+
+
+// Wrap every letter in a span
+$('.ml11 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0,$(".ml11 .letters").width()],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 6000,
+    offset: '-=775',
+    delay: function(el, i) {
+      return 34 * (i+1)
+    }
+  }).add({
+    targets: '.ml11',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
+
+  // var controller = new ScrollMagic.Controller();
+  
+  // // create a scene
+  // new ScrollMagic.Scene({
+  //         duration: 100,    // the scene should last for a scroll distance of 100px
+  //         offset: 50        // start this scene after scrolling for 50px
+  //     })
+  //     .setPin("#about") // pins the element for the the scene's duration
+  //     .addTo(controller);
+  
